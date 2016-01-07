0c7cf491410ab2d8e7dffc24d67582f3c3a85709
Wes Mason <wes@1stvamp.org>
2016-01-07 00:17:58 +0000

    Fleshed out install hook

--- reactive/errbot.py
+++ reactive/errbot.py
@@ -1,6 +1,24 @@
+from os import makedirs, path
+from subprocess import check_call
+
+from charmhelpers import fetch
+from charmhelpers.core import hookenv
+from charmhelpers.contrib.python import packages
+
 from charms.reactive import hook
+
+
+VENV_PATH = '/srv/errbot/venv'
 
 
 @hook('install')
 def install():
-    pass
+    hookenv.status_set('maintenance', 'Installing packages')
+    fetch.apt_install(['python3', 'python3-pip'])
+
+    if not path.exists(VENV_PATH):
+        makedirs(path.dirname(VENV_PATH))
+        check_call(['python3 venv', VENV_PATH])
+
+    version = hookenv.config('version')
+    packages.pip_install('errbot=={}'.format(version), venv=VENV_PATH)
