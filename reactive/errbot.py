797325c1c6ea0e12b862a43bb69286894a3b5726
Wes Mason <wes@1stvamp.org>
2016-01-07 11:43:52 +0000

    Fixed venv package install

--- reactive/errbot.py
+++ reactive/errbot.py
@@ -1,8 +1,10 @@
+from errno import EEXIST
 from os import makedirs, path
 from subprocess import check_call
 
 from charmhelpers import fetch
 from charmhelpers.core import hookenv
+from charmhelpers.core.host import lsb_release
 from charmhelpers.contrib.python import packages
 
 from charms.reactive import hook
@@ -11,14 +13,44 @@ from charms.reactive import hook
 VENV_PATH = '/srv/errbot/venv'
 
 
-@hook('install')
+@hook('config-changed')
 def install():
     hookenv.status_set('maintenance', 'Installing packages')
-    fetch.apt_install(['python3', 'python3-pip'])
+    codename = lsb_release()['DISTRIB_CODENAME']
+    if codename == 'trusty':
+        venv_pkg = 'python3.4-venv'
+    elif codename == 'xenial':
+        venv_pkg = 'python3.5-venv'
+    else:
+        venv_pkg = 'python3-venv'
+    apt_packages = [
+        'python3',
+        'python3-pip',
+        'libssl-dev',
+        'libffi-dev',
+        'python3-dev',
+        venv_pkg,
+    ]
+    fetch.apt_install(fetch.filter_installed_packages(apt_packages))
 
-    if not path.exists(VENV_PATH):
-        makedirs(path.dirname(VENV_PATH))
-        check_call(['python3 -m venv', VENV_PATH])
+    if not path.exists(path.join(VENV_PATH, 'bin')):
+        base_path = path.dirname(VENV_PATH)
+        try:
+            makedirs(base_path)
+        except OSError as e:
+            if e.errno == EEXIST and path.isdir(base_path):
+                pass
+            else:
+                raise
 
+        check_call(['/usr/bin/python3', '-m', 'venv', VENV_PATH])
+
+    hookenv.status_set('maintenance',
+                       'Installing configured version of errbot')
     version = hookenv.config('version')
+    if not version:
+        hookenv.log('version not set, skipping install of errbot',
+                    level='WARNING')
+        return
+
     packages.pip_install('errbot=={}'.format(version), venv=VENV_PATH)
