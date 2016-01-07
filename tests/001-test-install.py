aa3e29042fa612d70db02c6a92a9ae5a660a1c50
Wes Mason <wes@1stvamp.org>
2016-01-07 11:44:16 +0000

    Add config template and basic amulet test

---
+++ tests/001-test-install.py
@@ -0,0 +1,7 @@
+import amulet
+d = amulet.Deployment()
+d.add('errbot')
+d.configure('errbot', {'version': '3.2.2'})
+d.setup()
+d.sentry.wait()
+print(d.setry['errbot/0'].run('/srv/errbot/venv/bin/errbot'))
